fe3e2123088784e05e7579c517720b8319c80715
khoironi2 <khoironi7887@students.amikom.ac.id>
2020-12-28 17:53:10 +0700

    t

---
+++ application/controllers/dokter/layanan_pasien/Rekam_medis.php
@@ -0,0 +1,37 @@
+<?php
+
+class Rekam_medis extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->cek_status();
+        $this->CI = &get_instance();
+        if ($this->CI->router->fetch_class() != "login") {
+            // session check logic here...change this accordingly
+            if ($this->CI->session->userdata['level'] == 'admin') {
+                redirect('admin/dashboard');
+            } elseif ($this->CI->session->userdata['level'] == 'pasien') {
+                redirect('pasien/dashboard');
+            }
+        }
+    }
+
+    public function index()
+    {
+        $data = [
+            'title' => 'Sistem informasi klinik pelayanan hewan',
+            'halaman' => 'Data | Antrian Pasien',
+            'icon' => 'fas fa-pen-square',
+            'user' => $this->db->get_where('tbl_users', ['email' => $this->session->userdata('email')])->row_array(),
+            'antriansteril' => $this->Antrian_pasien_model->getAllSterilInDokter(),
+            'antrianvaksin' => $this->Antrian_pasien_model->getAllVaksinInDokter()
+        ];
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/topbar_dokter');
+        $this->load->view('templates/sidebar_dokter');
+        $this->load->view('dokter/layanan_pasien/antrian_pasien/index');
+        $this->load->view('templates/footer_dokter');
+    }
+}
